3db84c55e272ecb9c5451237031c9d5bdaf76b5a
Michael Wettstein <chischte@gmx.ch>
2020-05-16 07:07:38 +0200

    Refactor variables

--- EEPROM_Counter.h
+++ EEPROM_Counter.h
@@ -16,7 +16,6 @@
  * *****************************************************************************
  TODO:
  CLEAN UP THE WHOLE CODE
- CHANGE BYTES BACK TO BYTES (NOT INT)
  */
 
 #ifndef EEPROM_Counter_h
@@ -28,33 +27,32 @@ class EEPROM_Counter {
 public:
   // FUNTIONS:
   EEPROM_Counter();
-  void setup(int eepromMinAddress, int eepromMaxAddress, int numberOfValues);
-  void countOneUp(int valueNumber); // increases the value by one
-  void set(int valueNumber, long newValue); // sets value
-  long getValue(int valueNumber); // returns current value
-  void printDebugInformation(); // extensively prints what happens with the values
-  void setAllZero(); // resets all counters
+  void setup(int eeprom_min_address, int eeprom_max_address, int number_of_values);
+  void count_one_up(int value_number); // increases the value by one
+  void set(int value_number, long new_value); // sets value
+  long get_value(int value_number); // returns current value
+  void print_debug_information(); // extensively prints what happens with the values
+  void set_all_zero(); // resets all counters
 
   // VARIABLES:
   // n.a.
 
 private:
   // FUNCTIONS:
-  long eepromRead(int sourceAdress);
-  void eepromWrite(long newValue, int destinationAddress);
-  void eepromMonitorWriteCycles();
-  void eepromMoveStorageLocation();
-  int calculateAddress(int valueNumber);
+  long eeprom_read(int sourceAdress);
+  void eeprom_write(long new_value, int destination_address);
+  void eeprom_monitor_write_cycles();
+  void eeprom_move_storage_location();
+  int calculate_address(int value_number);
 
   // VARIABLES:
-  byte _bytesForWriteCounter=4;
-  int _storeLocation;
-  int _minValueStorelocation;
-  int _eepromMinAddress;
-  int _maxStoreLocation;
-  int _storeWriteCounterCounter;
-  int _numberOfValues;
-  long _numberOfWriteCycles;
-
+  byte _bytes_for_write_counter = 4;
+  int _store_location;
+  int _min_value_store_location;
+  int _eeprom_min_address;
+  int _max_store_location;
+  int _store_write_counter_counter;
+  int _number_of_values;
+  long _number_of_write_cycles;
 };
 #endif
